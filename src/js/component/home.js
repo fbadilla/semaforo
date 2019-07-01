5b23080a2746a44d88eaefa1a110b6b791da4a25
fbadilla <f.badilla228@gmail.com>
2019-07-01 15:38:07 +0000

    agrego onclick

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -5,21 +5,32 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export class Home extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			red: true,
+			yelow: true,
+			green: true
+		};
+	}
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+			<div className="container">
+				<div id="traffic">
+					<div
+						className={
+							"red ligth " + (this.state.red ? "selected" : "")
+						}
+					/>
+					<div
+						className="yelow ligth "
+						onclick={() => (this.state.yelow ? "selected" : "")}
+					/>
+					<div
+						className="green ligth "
+						onclick={() => (this.state.yelow ? "selected" : "")}
+					/>
+				</div>
 			</div>
 		);
 	}
