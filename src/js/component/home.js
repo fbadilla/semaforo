a96c0c1fd5c6e7e56800a0a22276680f64a171ef
fbadilla <f.badilla228@gmail.com>
2019-07-01 18:57:18 +0000

    final semaforo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,15 +2,14 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import PropTypes from "prop-types";
 
 //create your first component
 export class Home extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			red: true,
-			yelow: true,
-			green: true
+			selected: ""
 		};
 	}
 	render() {
@@ -19,19 +18,39 @@ export class Home extends React.Component {
 				<div id="traffic">
 					<div
 						className={
-							"red ligth " + (this.state.red ? "selected" : "")
+							"red ligth " +
+							(this.state.selected == "red" ? "selected" : "")
 						}
+						onClick={() => {
+							this.setState({ selected: "red" });
+						}}
 					/>
 					<div
-						className="yelow ligth "
-						onclick={() => (this.state.yelow ? "selected" : "")}
+						className={
+							"yelow ligth " +
+							(this.state.selected == "yelow" ? "selected" : "")
+						}
+						onClick={() => {
+							this.setState({ selected: "yelow" });
+						}}
 					/>
 					<div
-						className="green ligth "
-						onclick={() => (this.state.yelow ? "selected" : "")}
+						className={
+							"green ligth " +
+							(this.state.selected == "green" ? "selected" : "")
+						}
+						onClick={() => {
+							this.setState({ selected: "green" });
+						}}
 					/>
 				</div>
 			</div>
 		);
 	}
 }
+
+Home.propTypes = {
+	a: PropTypes.string,
+	b: PropTypes.string,
+	c: PropTypes.string
+};
